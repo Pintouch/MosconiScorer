9c6cc08628c5e1d9477a9deedd2fbff6eea831b2
Adrien PINTO <adrien.pinto@gmail.com>
2017-06-21 08:58:58 +0200

    Initial Commit

---
+++ MosconiScorer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MosconiScorer
+//
+//  Created by Pintouch on 21/06/2017.
+//  Copyright © 2017 Dashpool. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
